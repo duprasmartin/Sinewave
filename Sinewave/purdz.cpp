5b3549f6ed0b604c547b7282b43b04af9823dcf7
Martin Dupras <martindupras@gmail.com>
2019-03-14 22:40:39 +0000

    more cleanup

---
+++ Sinewave/purdz.cpp
@@ -0,0 +1,15 @@
+//
+//  purdz.cpp
+//  Sinewave
+//
+//  Created by Martin Dupras on 14/03/2019.
+//  Copyright © 2019 Martin Dupras. All rights reserved.
+//
+
+#include "purdz.hpp"
+
+
+double calculateSinusoid (int t, double omega) {
+    float val = sin((t*omega*TWOPI/360));
+    return val;
+}
---
+++ Sinewave/purdz.hpp
@@ -0,0 +1,14 @@
+//
+//  purdz.hpp
+//  Sinewave
+//
+//  Created by Martin Dupras on 14/03/2019.
+//  Copyright © 2019 Martin Dupras. All rights reserved.
+//
+
+#ifndef purdz_hpp
+#define purdz_hpp
+
+#include <stdio.h>
+
+#endif /* purdz_hpp */
