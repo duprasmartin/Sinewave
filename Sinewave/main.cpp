640d681462661fd2e81f17629b9f5bde6e440245
Martin Dupras <martindupras@gmail.com>
2019-03-14 16:23:38 +0000

    Initial Commit

---
+++ Sinewave/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sinewave
+//
+//  Created by Martin Dupras on 14/03/2019.
+//  Copyright © 2019 Martin Dupras. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
